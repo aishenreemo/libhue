2692e517d5cfc98a1ac99f8145386533cc54ae1d
Aishen <aish3n@pm.me>
2023-10-15 17:45:03 +0800

    test: update test

--- test.c
+++ test.c
@@ -1,7 +1,15 @@
+#include <stdio.h>
+#include <ansi.h>
 #include <hue.h>
 
-int main(
-	)
-{
-	my_function(0, 0, 0);
+int main() {
+	struct hue_t colors[3];
+	const char *colors_base16[3] =
+		{ HUE_KIZU_RED, HUE_KIZU_BLUE, HUE_KIZU_CYAN };
+
+	for (int i = 0; i < 3; i++) {
+		base16_to_hue(colors + i, colors_base16[i]);
+		printf("Hex: %s\nColor {r: %d, g: %d, b: %d}\n\n",
+		       colors_base16[i], HUE_TO_ARGS(colors[i]));
+	}
 }
